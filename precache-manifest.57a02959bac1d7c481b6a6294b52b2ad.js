23e48cc4b1f2d3668caa8a301a7dc432bfaf8bd6
Kyubendo <kotigo111@gmail.com>
2020-07-19 22:29:20 +0300

    Updates

---
+++ precache-manifest.57a02959bac1d7c481b6a6294b52b2ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ce32c0b3ca11745c6cb0ef63d74c2b",
+    "url": "/brawlhalla-weapons-chart/index.html"
+  },
+  {
+    "revision": "39904606e4671e32a1f5",
+    "url": "/brawlhalla-weapons-chart/static/css/main.8ddcf192.chunk.css"
+  },
+  {
+    "revision": "f04c52cac975cf5a770e",
+    "url": "/brawlhalla-weapons-chart/static/js/2.966b0322.chunk.js"
+  },
+  {
+    "revision": "1938aa8702435f138be9250d407c3628",
+    "url": "/brawlhalla-weapons-chart/static/js/2.966b0322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39904606e4671e32a1f5",
+    "url": "/brawlhalla-weapons-chart/static/js/main.7ba2e690.chunk.js"
+  },
+  {
+    "revision": "0985c15127983db47cfb",
+    "url": "/brawlhalla-weapons-chart/static/js/runtime-main.45dde74f.js"
+  }
+]);
